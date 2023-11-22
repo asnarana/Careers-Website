1833849a845d6e5551406dbd2d99972c139800cb
asnarana <114116966+asnarana@users.noreply.github.com>
2023-11-22 19:00:32 +0000

    set up secret to hide database pw

--- database.py
+++ database.py
@@ -1,8 +1,8 @@
 from sqlalchemy import create_engine, text
-
+import os
 # setting up connection to mySQL database this database is stored on
 # mySQLalchemy server.
-db_connectionstring = "mysql+pymysql://xoyjqkx4z1xvgj3gk63b:pscale_pw_njgKUHfcq0uWckHF6y2G8fgxYhrVxpLXRkusIM5dRHn@aws.connect.psdb.cloud/ncsucareers?charset=utf8mb4"
+db_connectionstring = os.environ['SECRET_KEY']
 engine = create_engine(db_connectionstring,
                        connect_args={"ssl": {
                            "ssl_ca": "/etc/ssl/cert.pem",
@@ -18,9 +18,3 @@ def load_positions_from_db():
         for row in result.fetchall()
     ]
     return ncsupositions
-
-
-# database: ncsucareers
-# username: xoyjqkx4z1xvgj3gk63b
-# host: aws.connect.psdb.cloud
-# password: pscale_pw_njgKUHfcq0uWckHF6y2G8fgxYhrVxpLXRkusIM5dRHn
